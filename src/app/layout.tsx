269a2218ed7736f5e73ed94ab16d3f26c3e9a0df
nicoloprog <nicosubywrx@gmail.com>
2025-08-29 19:47:32 -0400

    meta fixing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
+import { HOME_OG_IMAGE_URL } from "@/lib/constants";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import cn from "classnames";
@@ -9,8 +9,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  title: `Création Diane`,
+  description: `Les belles créations de Diane`,
   openGraph: {
     images: [HOME_OG_IMAGE_URL],
   },
